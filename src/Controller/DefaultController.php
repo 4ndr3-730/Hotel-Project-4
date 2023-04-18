898bf9afceddb0a1bd7b598a73cdad742b77119a
4ndr3-730 <ldvernot@gmail.com>
2023-04-18 16:30:41 +0200

    ChambreController/Showdashboard

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ChambreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'show_home', methods: ['GET'])]
-    public function showHome(): Response
+    public function showHome(ChambreRepository $repository): Response
     {
         return $this->render('default/show_home.html.twig', []);
     }
